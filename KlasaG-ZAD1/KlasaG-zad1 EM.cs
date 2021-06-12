7b48c8366ccca4c012e3b8be8d7a250c410be49e
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-06-12 14:34:36 +0200

    Add files via upload

---
+++ KlasaG-ZAD1/KlasaG-zad1 EM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaG_ZAD1
+{
+     public class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Unesena vrijednost: ");
+
+
+            int vrijednost = Convert.ToInt32(Console.ReadLine());
+            KlasaG zbroj = new KlasaG();
+
+            KlasaG umnozak = new KlasaG();
+            Console.WriteLine("Zbroj unesenih vrijednosti: ");
+            zbroj.f50(vrijednost);
+
+
+            Console.Write("Umnozak unesenih vrijednosti ");
+            umnozak.Plus10(vrijednost);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ KlasaG-ZAD1/KlasaG.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasaG_ZAD1
+{
+    public class KlasaG
+    {
+        public void Plus10(int vrijednost)
+        {
+            vrijednost = vrijednost * 10;
+            
+            
+            
+            Console.WriteLine(vrijednost);
+        }
+        public void f50(int vrijednost)
+        {
+             vrijednost = 50;
+            Console.WriteLine(vrijednost);
+        }
+
+    }
+}
